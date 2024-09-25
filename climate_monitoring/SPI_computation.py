778a85630e1e77caf3ba2a5570f0831de2838553
BenjaminLeleu25 <benji.leleu9@gmail.com>
2024-09-25 15:34:43 +0700

    Update SPI code -> doc + func

--- climate_monitoring/SPI_computation.py
+++ climate_monitoring/SPI_computation.py
@@ -5,15 +5,23 @@ from pathlib import Path
 from loguru import logger
 from xclim.indices import standardized_precipitation_index
 
-from climate_monitoring.map_generator import map_computation
-from tests.config.ParseConfigFiles import ParseSPIData
+from ConfigurationFiles.ParseConfFiles import ParseSPIData
 
 
-def compute_SPI(dict_config: dict, dict_config_map: dict, input_path: str, output_path: str | Path,
-                output_path_map: str | Path, launch_year: int, format_netcdf: str):
+def compute_SPI(dict_config: dict,
+                input_path: str | Path,
+                filename: str,
+                launch_year: int) -> xr.Dataset:
     ''' Compute SPI data and save the file in a repo.
         > dict_config:
             - dict of all the information contained in the yaml file for the SPI part
+
+        > input_path:
+            - path of the valid NetCDF file for SPI computation.
+              It must have values within the interval [start_period, end_period] (minimum)
+
+        > filename:
+            - name of the NetCDF file to open
 
         > launch_year:
             - year of computation
@@ -23,55 +31,42 @@ def compute_SPI(dict_config: dict, dict_config_map: dict, input_path: str, outpu
 
         Args:
             dict_config [dict]
-            dict_config_map [dict]
             input_path [str]
-            output_path [str or Path]
-            output_path_map [str or Path]
             launch_year [int]
 
         Return:
-            None
+            SPI_ds [xr.Dataset]
     '''
 
     resultat = []
-    input_data, climdex, units, map_creator_list, start_periode, \
-        end_periode, spi_period, func, freq, method, loc = ParseSPIData(dict_config)
+    units, start_period, end_period, spi_period, \
+        func, freq, method, loc = ParseSPIData(dict_config)
 
-    years = np.arange(start_periode, launch_year+1, 1)
+    logger.info(f'Open netcdf files from {input_path}')
+    ds = xr.open_dataset(f"{input_path}/{filename}", engine='netcdf4')
 
-    logger.info(f'Open netcdf files from {input_path} for years {start_periode} - {years[-1]}')
-    for year in years:
-        ds = xr.open_dataset(f"{input_path}/{year}/{climdex}/{climdex}_{input_data}_VALUES.nc", engine='netcdf4')
-        resultat.append(ds)
-        global_ds = xr.concat(resultat, dim=('time'))
+    # ds = ds[climdex]
+    ds.attrs.update(units)
 
-    pr = global_ds[climdex]
-    pr.attrs.update(units)
-
-    for month, create_map in zip(spi_period, map_creator_list):
-        SPI_array = standardized_precipitation_index(pr,
+    for month in spi_period:
+        SPI_array = standardized_precipitation_index(ds,
                                                      freq=freq,
                                                      window=month,
                                                      dist=func,
                                                      method=method,
                                                      fitkwargs={"floc": loc},
-                                                     cal_start=f'{start_periode}-01-01',
-                                                     cal_end=f'{end_periode}-12-31'
+                                                     cal_start=start_period,
+                                                     cal_end=end_period
                                                      )
 
         SPI_array = SPI_array.drop_vars('prob_of_zero')
         SPI_array = SPI_array.rename('SPI')
         del SPI_array.attrs['time_indexer']
         del SPI_array.attrs['calibration_period']
-        SPI_array = SPI_array.assign_attrs({'start_calibration_period': start_periode})
-        SPI_array = SPI_array.assign_attrs({'end_calibration_period': end_periode})
-
-        filepath = f'{output_path}/SPI/{launch_year}/'
-        os.makedirs(filepath, exist_ok=True)
-        logger.info(f'Save {month}-month SPI file calculated in {filepath}')
+        SPI_array = SPI_array.assign_attrs({'start_calibration_period': start_period})
+        SPI_array = SPI_array.assign_attrs({'end_calibration_period': end_period})
         SPI_ds = SPI_array.to_dataset(promote_attrs=True)
-        SPI_ds = SPI_ds.sel(time=slice(SPI_ds.time.values[-12], SPI_ds.time.values[-1]))
-        SPI_ds.to_netcdf(f'{filepath}/{month}-month_SPI_VALUES.nc', 'w', format_netcdf, engine='netcdf4')
+        # Extract the last year only
+        # SPI_ds = SPI_ds.sel(time=slice(SPI_ds.time.values[-12], SPI_ds.time.values[-1]))
 
-        if create_map is True:
-            map_computation(SPI_ds, dict_config_map, f'{month}-month_SPI_VALUES', output_path_map)
+    return SPI_ds
